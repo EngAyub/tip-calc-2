982b2c1e3d9123ddc832c812ca5dd5378b816b0a
ENG AYUUB <moztaphem9@gmal.com>
2023-05-22 22:22:56 +0300

    TIP CALCULATOR DONE

--- script.js
+++ script.js
@@ -1,1 +1,23 @@
-console.log("hello");
+function calculator(num1, num2, operator) {
+    let result;
+
+    if (operator === "+") {
+      result = num1 + num2;
+    } else if (operator === "-") {
+      result = num1 - num2;
+    } else if (operator === "") {
+      result = num1 * num2;
+    } else if (operator === "/") {
+      result = num1 / num2;
+    } else {
+      console.log("Invalid operator.");
+      return;
+    }
+
+    console.log(The Answer: ${result});
+  }
+    calculator(20, 9, "+");
+    calculator(55, 5, "-"); 
+    calculator(100, 5, "*"); 
+    calculator(90, 5, "/"); 
+    calculator(90, 5, "%");
